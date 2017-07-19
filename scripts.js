65af9f2d8ce68550ce613e0cf9416634fb974abe
Aellon Krider <aellonkrider@gmail.com>
2017-07-19 12:47:11 -0400

    media queries

--- scripts.js
+++ scripts.js
@@ -3,15 +3,15 @@
 $(document).ready(function(){
   $('.slider').slick({
     mobileFirst: true,
-	  slidesToShow: 1,
+	slidesToShow: 1,
     slidesToScroll: 1,
     initialSlide: 0,
     arrows: false,    
     dots: false,
     infinite: true,
     speed: 5,
-    variableWidth: true,
     cssEase: 'ease',
     appendArrows: $('.button')
   });
 });
+
